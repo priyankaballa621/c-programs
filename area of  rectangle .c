2d103ee58311df555592b7263bad4d7f53a4e596
priyankaballa621 <ballapriyanka621@gmail.com>
2026-07-18 10:39:23 +0530

    Add files via upload

---
+++ area of  rectangle .c
@@ -0,0 +1,13 @@
+// area of rectangle
+#include<stdio.h>
+int main()
+{
+	int l,b,area;
+	printf("enter value of l");
+	scanf("%d",&l);
+	printf("enter value of b");
+	scanf("%d",&b);
+	area = l*b;
+	printf("Area of rectangle =%d cm \n",area);
+	return 0;
+}
