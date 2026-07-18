8cae03165e14ead9640ffe3dc78e8f2bd8bf6016
priyankaballa621 <ballapriyanka621@gmail.com>
2026-07-18 15:03:05 +0530

    Add files via upload

---
+++ if case . cprg.cpp
@@ -0,0 +1,21 @@
+// to cheek the given number is positive or negative 
+#include<stdio.h>
+int main()
+{
+	int x;
+	printf ("enter a number ");
+	scanf("%d",&x);
+	if (x>=0)
+	{
+		printf("given number is positive ");
+	}
+	else {
+	printf ("given number is negative "); 
+	}
+	return 0;
+}
+
+
+	
+	
+
