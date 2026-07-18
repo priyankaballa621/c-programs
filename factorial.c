0a762d4520708ff2b9e007c091de1bccf0c9085f
priyankaballa621 <ballapriyanka621@gmail.com>
2026-07-18 10:37:44 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int fact(int);
+int main()
+{
+	int x,n;
+	printf("enter number to find factorial");
+	scanf("%d",&n);
+	x=fact(n);
+	printf(" factorial of %d is %d",n,x);
+	return 0;
+}
+int fact(int n){
+	if(n==0)
+	return 1;
+	return(n*fact(n-1));
+}
