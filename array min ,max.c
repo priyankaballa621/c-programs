53ff027517358ec76ff7a24b9209054584501f4f
priyankaballa621 <ballapriyanka621@gmail.com>
2026-07-18 14:42:32 +0530

    Add files via upload

---
+++ array min ,max.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,min,max;
+	printf("enter array size");
+	scanf("%d",&n);
+	int a[n];
+	printf("enter array elements");
+	for(i=0;i<n;i++)
+	{
+	scanf("%d",&a[i]);
+	min=max=a[0];
+    }
+	for(i=1;i<n;i++){
+         if(a[i]<min)
+         min=a[i];
+         if(a[i]>max)
+         max=a[i];
+        }
+         printf("%d",min);
+         printf("\n%d",max);
+         return 0;
+         
+}
