4775549914fea17f565118f531ca19c3aa1d60e3
priyankaballa621 <ballapriyanka621@gmail.com>
2026-07-18 14:30:49 +0530

    Add files via upload

---
+++ area of square .c
@@ -0,0 +1,11 @@
+//area of square 
+#include<stdio.h>
+int main()
+{
+	int x,area;
+	printf("enter value  of x");
+	scanf("%d",&x);
+	area = x*x;
+	printf("Area of square=%d cm \n",area);
+	return 0;
+}
